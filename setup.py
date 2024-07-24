b589bbd6a5d570907e9548c4f1ac0c258562f14b
andrewbxy <xybai@hku.hk>
2024-07-24 13:43:36 +0800

    change name to resolve installation conflict

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization_9ch",
-    packages=['diff_gaussian_rasterization'],
+    name="diff_gaussian_rasterization_stg",
+    packages=['diff_gaussian_rasterization_stg'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="diff_gaussian_rasterization_stg._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
